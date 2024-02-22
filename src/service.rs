e0e6ad408b4cf8e82ae29fd626f42dd9c0ab471d
Paul Nettleton <k9@k9withabone.dev>
2024-02-22 16:35:16 -0600

    feat(service): add `cap_add/drop` and `cgroup(_parent)` fields

--- src/service.rs
+++ src/service.rs
@@ -9,9 +9,15 @@ pub mod image;
 pub mod platform;
 mod ulimit;
 
-use std::{net::IpAddr, time::Duration};
+use std::{
+    fmt::{self, Display, Formatter},
+    net::IpAddr,
+    str::FromStr,
+    time::Duration,
+};
 
-use indexmap::IndexMap;
+use compose_spec_macros::{DeserializeFromStr, SerializeDisplay};
+use indexmap::{IndexMap, IndexSet};
 use serde::{de, Deserialize, Deserializer, Serialize};
 use thiserror::Error;
 
@@ -127,6 +133,34 @@ pub struct Service {
     #[serde(default, skip_serializing_if = "CpuSet::is_empty")]
     pub cpuset: CpuSet,
 
+    /// Add additional container [**capabilities**(7)](https://man7.org/linux/man-pages/man7/capabilities.7.html).
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/05-services.md#cap_add)
+    #[serde(default, skip_serializing_if = "IndexSet::is_empty")]
+    pub cap_add: IndexSet<String>,
+
+    /// Drop container [**capabilities**(7)](https://man7.org/linux/man-pages/man7/capabilities.7.html).
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/05-services.md#cap_drop)
+    #[serde(default, skip_serializing_if = "IndexSet::is_empty")]
+    pub cap_drop: IndexSet<String>,
+
+    /// [Cgroup](https://man7.org/linux/man-pages/man7/cgroups.7.html) namespace to join.
+    ///
+    /// When unset, it is the container runtime's decision to select which cgroup namespace to use,
+    /// if supported.
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/05-services.md#cgroup)
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub cgroup: Option<Cgroup>,
+
+    /// Optional parent [cgroup](https://man7.org/linux/man-pages/man7/cgroups.7.html) for the
+    /// container.
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/05-services.md#cgroup_parent)
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub cgroup_parent: Option<String>,
+
     /// Specifies a build's container isolation technology.
     ///
     /// Supported values are platform specific.
@@ -186,6 +220,72 @@ impl PartialEq<u8> for Percent {
         self.0.eq(other)
     }
 }
+
+/// [Cgroup](https://man7.org/linux/man-pages/man7/cgroups.7.html) namespace to join.
+///
+/// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/05-services.md#cgroup)
+#[derive(SerializeDisplay, DeserializeFromStr, Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Cgroup {
+    /// Run the container in the Container runtime cgroup namespace.
+    Host,
+
+    /// Run the container in its own private cgroup namespace.
+    Private,
+}
+
+impl Cgroup {
+    /// [`Cgroup`] option as a static string slice.
+    #[must_use]
+    pub const fn as_str(&self) -> &'static str {
+        match self {
+            Self::Host => "host",
+            Self::Private => "private",
+        }
+    }
+}
+
+impl AsRef<str> for Cgroup {
+    fn as_ref(&self) -> &str {
+        self.as_str()
+    }
+}
+
+impl Display for Cgroup {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str(self.as_str())
+    }
+}
+
+impl From<Cgroup> for &'static str {
+    fn from(value: Cgroup) -> Self {
+        value.as_str()
+    }
+}
+
+impl FromStr for Cgroup {
+    type Err = ParseCgroupError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "host" => Ok(Self::Host),
+            "private" => Ok(Self::Private),
+            s => Err(ParseCgroupError(s.to_owned())),
+        }
+    }
+}
+
+impl TryFrom<&str> for Cgroup {
+    type Error = ParseCgroupError;
+
+    fn try_from(value: &str) -> Result<Self, Self::Error> {
+        value.parse()
+    }
+}
+
+/// Error returned when parsing a [`Cgroup`] from a string.
+#[derive(Error, Debug, Clone, PartialEq, Eq)]
+#[error("invalid cgroup option `{0}`, cgroup must be `host` or `private`")]
+pub struct ParseCgroupError(String);
 
 /// Deserialize `extra_hosts` field of [`Service`] and long [`Build`] syntax.
 ///
