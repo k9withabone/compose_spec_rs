c6d03cc12f7852d415cf62465e8eed47dc9bda63
Paul Nettleton <k9@k9withabone.dev>
2024-03-23 06:16:11 -0500

    feat(volume): add `driver` and `driver_opts` fields

--- src/volume.rs
+++ src/volume.rs
@@ -1,6 +1,7 @@
+use indexmap::IndexMap;
 use serde::{Deserialize, Serialize};
 
-use crate::{Extensions, Resource};
+use crate::{Extensions, MapKey, Resource, StringOrNumber};
 
 impl From<Volume> for Resource<Volume> {
     fn from(value: Volume) -> Self {
@@ -15,6 +16,20 @@ impl From<Volume> for Resource<Volume> {
 /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/07-volumes.md)
 #[derive(Serialize, Deserialize, Debug, Default, Clone, PartialEq)]
 pub struct Volume {
+    /// Which driver to use for this volume.
+    ///
+    /// Default and available values are platform specific.
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/07-volumes.md#driver)
+    #[serde(default, skip_serializing_if = "Option::is_none")]
+    pub driver: Option<String>,
+
+    /// Driver-dependent options.
+    ///
+    /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/07-volumes.md#driver_opts)
+    #[serde(default, skip_serializing_if = "IndexMap::is_empty")]
+    pub driver_opts: IndexMap<MapKey, StringOrNumber>,
+
     /// Extension values, which are (de)serialized via flattening.
     ///
     /// [compose-spec](https://github.com/compose-spec/compose-spec/blob/master/11-extension.md)
